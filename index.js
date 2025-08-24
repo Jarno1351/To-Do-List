8e011d7d288979627a4f7eb34f5f93199343c35d
Jarno1351 <jarno1351@gmail.com>
2025-08-25 01:50:09 +0800

    Main

---
+++ index.js
@@ -0,0 +1,58 @@
+const itemCont = document.getElementById(`items`);
+const input = document.getElementById(`inputItems`)
+let items = ["Delete Me!"]
+
+function renderItems(){
+    itemCont.innerHTML = null;
+
+    for(const [idx, value] of Object.entries(items)){
+        const container = document.createElement('div')
+        container.classList.add("container")
+
+        const text = document.createElement("p")
+        text.textContent = `${Number(idx) + 1}. ${value}`
+        text.classList.add("text")
+
+        const button = document.createElement("button")
+        button.textContent = "Delete"
+        button.classList.add("button")
+        button.onclick = () => deleteItems(idx)
+
+
+        
+        container.appendChild(text)
+        container.appendChild(button)
+        itemCont.appendChild(container)
+    }
+}
+function addItems(){
+    const value = input.value;
+    if(!value){
+        alert("Please enter a value!")
+    }
+    else{
+        items.push(value)
+        saveItems()
+        renderItems()
+    }
+    
+}
+
+function saveItems(){
+    const values = JSON.stringify(items)
+    localStorage.setItem("items", values)
+}
+
+function loadItems(){
+    let itemsVal = localStorage.getItem("items")
+    items = JSON.parse(itemsVal)
+    renderItems()
+}
+
+function deleteItems(idx){
+    items.splice(idx, 1)
+    saveItems()
+    renderItems()
+}
+
+addEventListener("DOMContentLoaded", loadItems())
